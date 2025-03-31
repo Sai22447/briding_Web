65c739f99cd6129452cdaea4b508695628f3bfd7
Sai22447 <umahobby2887@gmail.com>
2025-03-31 13:51:00 +0630

    first_project

---
+++ week2/chap6/favourite_numbers.py
@@ -0,0 +1,13 @@
+favourite_numbers = {'TL':4,'JJ':39,'IE':72,'SE':15}
+
+for name, number in favourite_numbers.items():
+    print(f"{name}'s favourite number is {number}")
+
+print("Items view")
+print(favourite_numbers.items())
+
+print("key view")
+print(favourite_numbers.keys())
+
+print("values")
+print(favourite_numbers.values())
---
+++ week3/user.py
@@ -0,0 +1,8 @@
+user_0 = {'username':'epucci',
+          'first':'enrico',
+          'last':'pucci'}
+
+for key, value in user_0.items():
+    print(f"key {key}")
+    print(f"value {value}")
+    print()
